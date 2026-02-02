eaa30c5a853f58daa9c5f6acfcddc301c24daa8c
erik-per-lidstrom <kirelidstrom@gmail.com>
2026-02-02 12:10:17 +0100

    added auth middel ware and types for it

---
+++ src/middleware/auth.middelware.ts
@@ -0,0 +1,49 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { AppError } from "../utils/app.error";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: { id: string; role: string };
+    }
+  }
+}
+
+export const protect = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  if (!token) {
+    throw new AppError("unauthorised", 401);
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+
+    const paload = decoded as JwtPayload;
+
+    req.user = { id: paload.id, role: paload.role };
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+type Roles = "user" | "modirator" | "admin";
+
+export const restrictTo = (...allowedRoles: Roles[]) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const user = req.user;
+
+    if (!user || !allowedRoles.includes(user.role as Roles)) {
+      throw new AppError("forbiden", 403);
+    }
+
+    next();
+  };
+};
